d103c84f1f5798d0956051a259511b1392fdd6ba
JuHambre <juanesquerdoroig@gmail.com>
2014-10-21 11:10:37 +0200

    First commit

---
+++ Assets/scripts/bottontitle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bottontitle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Se llama en cada frame
+	void OnGUI () {
+		if (GUI.Button (new Rect (Screen.width * 0.7f, Screen.height * 0.2f, 200, 50), "Empezar el juego")) {
+			Application.LoadLevel("selectlevel");
+		}
+	}
+}
---
+++ Assets/scripts/characterMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterMovement : MonoBehaviour {
+	public Animator characterAnimator;
+	public float maxSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis ("Horizontal");
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+		characterAnimator.SetFloat ("Velocidad", rigidbody2D.velocity.x); 
+	}
+}
