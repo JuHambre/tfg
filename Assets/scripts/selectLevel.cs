9672ee167cac4890ba79d0655996a71a7fab717b
JuHambre <juanesquerdoroig@gmail.com>
2014-10-23 10:53:15 +0200

    Iniciacion nivel final

--- Assets/scripts/selectLevel.cs
+++ Assets/scripts/selectLevel.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class selectLevel : MonoBehaviour {
 	public Texture coutTexture;
+	public Texture finishTexture;
 	// Use this for initialization
 	void Start () {
 	
@@ -16,6 +17,9 @@ public class selectLevel : MonoBehaviour {
 	void OnGUI () {
 		if (GUI.Button (new Rect (Screen.width * 0.1f, Screen.height * 0.2f, coutTexture.width*0.5f, coutTexture.height*0.5f), coutTexture)) {
 			Application.LoadLevel("cout");
+		}
+		else if (GUI.Button (new Rect (Screen.width * 0.4f, Screen.height * 0.7f, coutTexture.width*0.5f, coutTexture.height*0.5f), finishTexture)) {
+			Application.LoadLevel("finishlevel");
 		}
 	}
 }
